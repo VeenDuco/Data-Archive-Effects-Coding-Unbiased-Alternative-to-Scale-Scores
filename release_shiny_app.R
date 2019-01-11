a4d2613df8159973927ab101643f0473a6cb5401
Veen <d.veen@uu.nl>
2019-01-11 09:39:23 +0100

    Revert "Revert "Initial commit""
    This reverts commit d973d74b4bb05a1b36c5a5d0bbc8d7ea58d4f1d8.

---
+++ release_shiny_app.R
@@ -0,0 +1,2 @@
+rsconnect::deployApp(appName = "Effects-Coding",'C:/Users/5507553/surfdrive/PhD werk/Projecten/simulation factor and sum scores/Data Archive/Simulations_EC_AS/shinyapp',
+                     account="utrecht-university")
